01be142b81e00a0ed83a8c45886269459b3665e9
Jitisha Khede <khedejitisha2004@gmail.com>
2024-07-21 16:49:39 +0530

    accepted

---
+++ Tree/medium/116_Populating_Next_Right_Pointers_in_Each_Node.cpp
@@ -0,0 +1,46 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        queue<Node*> q;
+        Node* node;
+        q.push(root);
+        q.push(NULL);
+        while(!q.empty()){
+           vector<int> level;
+            int qlen = q.size();
+            for(int i=0;i<qlen;i++){
+                node = q.front();
+                q.pop();
+                if(node!=NULL){
+                    level.push_back(node->val);
+                    node->next = q.front();
+                    if(node->left) q.push(node->left);
+                    if(node->right) q.push(node->right);    
+                } 
+            }
+            if(!level.empty()){
+                q.push(NULL);
+            }
+            
+        }
+        return root;
+    }
+};
