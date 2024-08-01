36416e6efbe9fee236018b403f89283b32b3854f
Jitisha Khede <khedejitisha2004@gmail.com>
2024-08-01 12:07:20 +0530

    accepted

---
+++ daily problems/august/2678_Number_of_Senior_Citizens.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int count = 0;
+        for(int i=0;i<details.size();i++){
+            if(details[i][11]-'0'==6 && details[i][12]-'0'==0) continue;
+            else if((details[i][11])-'0'>=6 && (details[i][12])-'0'>=0){
+                count++;
+            }
+        }
+        return count;
+    }
+};
