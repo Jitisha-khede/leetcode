7f6a2e3aaf914eae0f721c77c4908238cf95d7aa
Jitisha Khede <khedejitisha2004@gmail.com>
2025-07-10 01:39:28 +0530

    added optimal approach to solve problem

--- Array/medium/128_Longest_Consecutive_Sequence.cpp
+++ Array/medium/128_Longest_Consecutive_Sequence.cpp
@@ -1,19 +1,40 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        if(nums.size()==0 || nums.size()==1) return nums.size();
-        sort(nums.begin(),nums.end());
-        int maxi=0,count=0;
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i]==nums[i+1]) continue;
-            if(nums[i]+1==nums[i+1]){
-                count++;
-            }
-            maxi = max(maxi,count);
-            if(nums[i]+1!=nums[i+1]){    
-                count=0;
+
+        //BETTER
+        // int c = 0,ans=0;
+        // sort(nums.begin(),nums.end());
+        // for(int i=0;i<nums.size()-1;i++){
+        //     if(nums[i]==nums[i+1]) continue;
+        //     else if(nums[i]+1==nums[i+1]){
+        //         c++;
+        //         ans = max(c,ans);
+        //     }
+        //     else{
+        //         c=0;
+        //     }
+        // }
+        // return ans+1;
+
+        //OPTIMAL
+        if(nums.size()==0)return 0;
+        int ans = 0;
+        unordered_set<int> st;
+        for(int i=0;i<nums.size();i++){
+            st.insert(nums[i]);
+        }
+        for(auto it : st){
+            if(st.find(it-1)==st.end()){
+                int c=0;
+                int x = it;
+                while(st.find(x+1)!=st.end()){
+                    c++;
+                    x++;
+                }
+                ans = max(ans,c);
             }
         }
-        return maxi+1;
+        return ans+1;
     }
 };
