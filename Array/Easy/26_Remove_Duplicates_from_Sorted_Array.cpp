5d635833cfaee5305503bffe5c600e8ef5e3d0ca
Jitisha Khede <khedejitisha2004@gmail.com>
2025-07-18 01:43:01 +0530

    added optimal solution of problem

--- Array/Easy/26_Remove_Duplicates_from_Sorted_Array.cpp
+++ Array/Easy/26_Remove_Duplicates_from_Sorted_Array.cpp
@@ -1,3 +1,4 @@
+//BRUTE FORCE
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
@@ -8,5 +9,24 @@ public:
             i++;
         }
         return i;
+    }
+};
+
+//OPTIMAL
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int r = 1;
+        int l = 0;
+        int k = 1;
+        while(r<nums.size()){
+            if(nums[l]==nums[r]) r++;
+            else{
+                nums[l+1] = nums[r];
+                l++;
+                k++;
+            }
+        }
+        return k;
     }
 };
