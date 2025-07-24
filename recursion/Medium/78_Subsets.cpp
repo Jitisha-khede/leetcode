03c4a9fca2d21cac7757bb30e7cf817f9bf3190a
Jitisha Khede <khedejitisha2004@gmail.com>
2025-07-25 03:04:07 +0530

    revised code

--- recursion/Medium/78_Subsets.cpp
+++ recursion/Medium/78_Subsets.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
-    void solve(vector<vector<int>> &ans, vector<int> &nums, vector<int> output, int i){
+    void solve(vector<vector<int>> &ans, vector<int> &nums, vector<int> &temp, int i){
         if(i>=nums.size()){
-            ans.push_back(output);
+            ans.push_back(temp);
             return;
         }
 
-        //exclude
-        solve(ans,nums,output,i+1);
+        temp.push_back(nums[i]);
+        solve(ans,nums,temp,i+1);
+        temp.pop_back();
+        solve(ans,nums,temp,i+1);
 
-        //include
-        int elem = nums[i];
-        output.push_back(elem);
-        solve(ans,nums,output,i+1);
     }
 
     vector<vector<int>> subsets(vector<int>& nums) {
