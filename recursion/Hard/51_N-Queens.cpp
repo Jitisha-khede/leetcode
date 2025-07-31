4495f02fd2c6a2f2afa1065f097b1098d3dd019b
Jitisha Khede <khedejitisha2004@gmail.com>
2025-08-01 02:43:25 +0530

    added optimal solution

--- recursion/Hard/51_N-Queens.cpp
+++ recursion/Hard/51_N-Queens.cpp
@@ -1,3 +1,4 @@
+//BRUTE FORCE
 class Solution {
 public:
     bool isSafe( int row,int col,vector<string> &board, int n){
@@ -45,6 +46,41 @@ public:
         board[i]=s;
        }
        solve(board,n,0,ans);
+       return ans;
+    }
+};
+//OPTIMAL
+class Solution {
+public:
+    void solve(vector<string> &board,int n,int col,vector<vector<string>> &ans,
+    vector<int> &lower, vector<int> &upper, vector<int> &side){
+        if(col==n){
+            ans.push_back(board);
+            return;
+        }
+        for(int row = 0;row<n;row++){
+            if(side[row]==0 && lower[row+col]==0 && upper[n-1+row-col]==0){
+                board[row][col]='Q';
+                side[row]=1;
+                lower[row+col]=1;
+                upper[n-1+row-col]=1;
+                solve(board,n,col+1,ans,lower,upper,side);
+                board[row][col]='.';
+                side[row]=0;
+                lower[row+col]=0;
+                upper[n-1+row-col]=0;
+            }
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+       vector<vector<string>> ans;
+       vector<string> board(n);
+       vector<int> lower(2*n-1,0),upper(2*n-1,0),side(n,0);
+       string s(n,'.');
+       for(int i=0;i<n;i++){
+        board[i]=s;
+       }
+       solve(board,n,0,ans,lower,upper,side);
        return ans;
     }
 };
