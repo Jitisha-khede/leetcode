ab03cd1ec8aca2da63a9aca67be9d624884525b8
Jitisha Khede <khedejitisha2004@gmail.com>
2025-08-08 10:46:25 +0530

    added optimal solution

--- Linked List/Medium/328_Odd_Even_Linked_List.cpp
+++ Linked List/Medium/328_Odd_Even_Linked_List.cpp
@@ -37,3 +37,24 @@ public:
         return head;
     }
 };
+
+//OPTIMAL
+
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(head==NULL || head->next==NULL || head->next->next==NULL) return head;
+        ListNode* evenHead = head->next;
+        ListNode* even = head->next;
+        ListNode* odd = head;
+
+        while((even!=NULL && even->next!=NULL)){
+            odd->next = even->next;
+            even->next = even->next->next;
+            odd = odd->next;
+            even = even->next;
+        }
+        odd->next = evenHead;
+        return head;
+    }
+};
