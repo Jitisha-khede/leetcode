c8761c147cc052e4d8758d9cdb960e9af2dcce28
Jitisha Khede <khedejitisha2004@gmail.com>
2025-08-08 10:47:57 +0530

    added optimal using slow fast pointer

--- Linked List/Medium/2095_Delete_the_Middle_Node_of_a_Linked_List.cpp
+++ Linked List/Medium/2095_Delete_the_Middle_Node_of_a_Linked_List.cpp
@@ -31,3 +31,20 @@ public:
         return head;
     }
 };
+// OPTIMAL
+class Solution {
+public:
+    ListNode* deleteMiddle(ListNode* head) {
+        if(head==NULL || head->next==NULL) return NULL;
+        ListNode* prev = NULL;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast!=NULL && fast->next!=NULL){
+            prev = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        prev->next = slow->next;
+        return head;
+    }
+};
