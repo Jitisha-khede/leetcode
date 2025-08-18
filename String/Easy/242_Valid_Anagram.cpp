f26768bf1115db40fbfbc535e4b401400bf2be2d
Jitisha Khede <khedejitisha2004@gmail.com>
2025-08-19 01:55:36 +0530

    accepted: added another approach

--- String/Easy/242_Valid_Anagram.cpp
+++ String/Easy/242_Valid_Anagram.cpp
@@ -1,3 +1,4 @@
+//APPROACH-1
 class Solution {
 public:
     bool isAnagram(string s, string t) {
@@ -5,5 +6,23 @@ public:
         sort(t.begin(),t.end());
         if(s!=t) return false;
         else return true;
+    }
+};
+//APPROACH-2
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()) return false;
+        unordered_map<char,int> mp;
+
+        for(int i=0;i<s.size();i++){
+            mp[s[i]]++;
+            mp[t[i]]--;
+        }
+        for(auto it: mp){
+            if(it.second!=0) return false;
+        }
+        return true;
     }
 };
