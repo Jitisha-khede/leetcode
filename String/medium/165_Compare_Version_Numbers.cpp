e00b1a0fe775657c1f5c4f810bf945363d8b18c9
Jitisha Khede <khedejitisha2004@gmail.com>
2025-08-21 00:16:04 +0530

    acccepted

---
+++ String/medium/165_Compare_Version_Numbers.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int compareVersion(string v1, string v2) {
+        v1+='.';
+        v2+='.';
+        string t1,t2;
+        int n1 = v1.size();
+        int n2 = v2.size();
+        int i=0,j=0;
+
+        while(i<n1 || j<n2){
+            long long num1=0, num2=0;
+            while(i<n1 && v1[i]!='.'){
+                num1=num1*10+v1[i]-'0';
+                i++;
+            }
+            while(j<n2 && v2[j]!='.'){
+                num2 = num2*10 +v2[j]-'0';
+                j++;
+            }
+            if(num1<num2) return -1;
+            else if(num1>num2) return 1;
+            else{
+                i++;
+                j++;
+            }
+        }
+        return 0;
+    }
+};
